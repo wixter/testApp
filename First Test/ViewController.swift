1d775d3e03bb7012e4de2c1f588fc15c2f8963f6
Santi Maestre <webmaster.yago@icloud.com>
2017-09-15 12:33:14 +0200

    Primera nota

--- First Test/ViewController.swift
+++ First Test/ViewController.swift
@@ -10,9 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCounter = 0
+
+    @IBAction func pushButton(_ sender: Any) {
+        
+        theLabel.text = "Why did you push it?"
+        tapCounter = tapCounter + 1
+        if tapCounter == 2 || tapCounter == 3 {
+            theLabel.text = "Stop!"
+        }
+        if tapCounter == 4 || tapCounter == 5{
+            theLabel.text = "STOP!"
+        }
+        if tapCounter == 6 || tapCounter == 7 || tapCounter == 8 || tapCounter == 9{
+            theLabel.text = "OK... Cool... Keep going"
+        }
+        if tapCounter == 10 {
+            theLabel.text = "You did it 10 times..."
+        }
+        if tapCounter > 10 {
+            theLabel.text = "You're gonna die💀"
+        }
+    
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
